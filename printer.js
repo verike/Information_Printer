fea2067d232faa77b30cb97358cf83796a3bda94
Victor Erike <verike7@gmail.com>
2022-06-27 16:25:29 +0100

    first code push

---
+++ printer.js
@@ -0,0 +1,8 @@
+
+//To request for informations
+
+personsname = prompt("Enter your full name...");
+height = prompt("Enter your height in (Cm)...");
+country = prompt("What country are your from ?");
+
+alert("Hi "+personsname+", \r\n From the information you provided, your height is "+height+"Cm \r\n and you are from "+country+". :)");
